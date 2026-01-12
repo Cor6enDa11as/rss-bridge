b695b6296f892182814526d1472699168d2ece81
Cor6enDa11as <145114275+Cor6enDa11as@users.noreply.github.com>
2026-01-13 02:51:04 +0300

    Update PikabuBridge.php
    2

--- bridges/PikabuBridge.php
+++ bridges/PikabuBridge.php
@@ -87,6 +87,7 @@ class PikabuBridge extends BridgeAbstract
         if ($my_cookies) {
             $header[] = 'Cookie: ' . $my_cookies;
             $header[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0';
+			$header[] = 'X-Requested-With: XMLHttpRequest';
         }
 
         // Передаем массив $header вторым аргументом
