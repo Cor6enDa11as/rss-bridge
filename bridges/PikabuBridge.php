fce86d057ddb3bffa2df987c599e43af655ac079
Cor6enDa11as <145114275+Cor6enDa11as@users.noreply.github.com>
2026-01-13 02:38:15 +0300

    Update PikabuBridge.php
    pikabu

--- bridges/PikabuBridge.php
+++ bridges/PikabuBridge.php
@@ -80,7 +80,17 @@ class PikabuBridge extends BridgeAbstract
     {
         $link = $this->getURI();
 
-        $text_html = getContents($link);
+        // Формируем заголовки из переменной Northflank
+        $my_cookies = getenv('PIKABU_COOKIES');
+        $header = [];
+        
+        if ($my_cookies) {
+            $header[] = 'Cookie: ' . $my_cookies;
+            $header[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0';
+        }
+
+        // Передаем массив $header вторым аргументом
+        $text_html = getContents($link, $header);
         $text_html = iconv('windows-1251', 'utf-8', $text_html);
         $html = str_get_html($text_html);
 
